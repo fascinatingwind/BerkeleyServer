41aa06b29f9e1bfab1a8997a9b432de94e846529
R1nk <fascwind@gmail.com>
2021-10-21 11:22:33 +0500

    implement network helper

--- BerkeleyServer/BerkeleyClient/src/BerkeleyClient.cpp
+++ BerkeleyServer/BerkeleyClient/src/BerkeleyClient.cpp
@@ -4,34 +4,10 @@
 #include <string>
 #include <algorithm>
 
-#include "Socket.h"
-#include "Connection.h"
-#include "UDPSocket.h"
-#include "UDPConnection.h"
-#include "TCPSocket.h"
-#include "TCPConnection.h"
+#include "NetworkHelper.h"
 
 std::string GetString(char *argv) {
     return {argv, strlen(argv)};
-}
-
-bool IsTCP(const std::string &type) {
-    const auto fit = type.find("tcp");
-    return fit != std::string::npos;
-}
-
-Network::SocketPtr MakeSocket(const std::string &type, const std::string &address, const std::string &port) {
-    if (IsTCP(type))
-        return std::make_shared<Network::UDPSocket>(port, address);
-    else
-        return std::make_shared<Network::TCPSocket>(port, address);
-}
-
-Network::ConnectionPtr MakeConnection(const std::string &type) {
-    if (IsTCP(type))
-        return std::make_shared<Network::TCPConnection>();
-    else
-        return std::make_shared<Network::UDPConnection>();
 }
 
 int main(int argc, char *argv[]) {
@@ -42,10 +18,11 @@ int main(int argc, char *argv[]) {
     auto type_arg = GetString(argv[3]);
     std::transform(type_arg.begin(), type_arg.end(), type_arg.begin(),
                    [](unsigned char c) { return std::tolower(c); });
-    const auto socket = MakeSocket(type_arg, GetString(argv[2]), GetString(argv[1]));
+    const auto socket = Network::NetworkHelper::MakeSocket(type_arg, GetString(argv[2]),
+                                                           GetString(argv[1]));
     socket->CreateSocket();
 
-    auto connection = MakeConnection(type_arg);
+    auto connection = Network::NetworkHelper::MakeConnection(type_arg);
     connection->Connect(socket);
     connection->SetBuffer(GetString(argv[4]));
     connection->Write();
