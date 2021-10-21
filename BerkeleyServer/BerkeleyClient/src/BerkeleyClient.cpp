b627edb735b7dce2c177fde045bf176b5ddc39e6
R1nk <fascwind@gmail.com>
2021-10-21 11:13:39 +0500

    add client side

--- BerkeleyServer/BerkeleyClient/src/BerkeleyClient.cpp
+++ BerkeleyServer/BerkeleyClient/src/BerkeleyClient.cpp
@@ -2,24 +2,55 @@
 
 #include <cstring>
 #include <string>
+#include <algorithm>
 
+#include "Socket.h"
+#include "Connection.h"
+#include "UDPSocket.h"
+#include "UDPConnection.h"
 #include "TCPSocket.h"
 #include "TCPConnection.h"
-#include "PortHelper.h"
+
+std::string GetString(char *argv) {
+    return {argv, strlen(argv)};
+}
+
+bool IsTCP(const std::string &type) {
+    const auto fit = type.find("tcp");
+    return fit != std::string::npos;
+}
+
+Network::SocketPtr MakeSocket(const std::string &type, const std::string &address, const std::string &port) {
+    if (IsTCP(type))
+        return std::make_shared<Network::UDPSocket>(port, address);
+    else
+        return std::make_shared<Network::TCPSocket>(port, address);
+}
+
+Network::ConnectionPtr MakeConnection(const std::string &type) {
+    if (IsTCP(type))
+        return std::make_shared<Network::TCPConnection>();
+    else
+        return std::make_shared<Network::UDPConnection>();
+}
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        std::cout << "Please enter address and port and message" << std::endl;
+    if (argc < 5) {
+        std::cout << "Please enter address, port, protocol and message" << std::endl;
     }
+// may crash if no ascii symbol entered;
+    auto type_arg = GetString(argv[3]);
+    std::transform(type_arg.begin(), type_arg.end(), type_arg.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+    const auto socket = MakeSocket(type_arg, GetString(argv[2]), GetString(argv[1]));
+    socket->CreateSocket();
 
-    Network::TCPSocket socket(std::string(argv[2],strlen(argv[2])), std::string(argv[1],strlen(argv[1])));
-    socket.CreateSocket();
-    Network::TCPConnection connection;
-    connection.Connect(socket);
-    connection.SetBuffer(std::string(argv[3], strlen(argv[3])));
-    connection.Write();
-    connection.Read();
-    const auto message = connection.GetBuffer();
+    auto connection = MakeConnection(type_arg);
+    connection->Connect(socket);
+    connection->SetBuffer(GetString(argv[4]));
+    connection->Write();
+    connection->Read();
+    const auto message = connection->GetBuffer();
     std::cout << message << std::endl;
     return EXIT_SUCCESS;
 }
