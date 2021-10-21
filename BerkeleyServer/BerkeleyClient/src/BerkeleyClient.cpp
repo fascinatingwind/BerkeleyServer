440f4ff0a9121429398d8fa10190af91fbbb1ee1
R1nk <fascwind@gmail.com>
2021-10-21 13:40:13 +0500

    little improve client

--- BerkeleyServer/BerkeleyClient/src/BerkeleyClient.cpp
+++ BerkeleyServer/BerkeleyClient/src/BerkeleyClient.cpp
@@ -13,21 +13,25 @@ std::string GetString(char *argv) {
 int main(int argc, char *argv[]) {
     if (argc < 5) {
         std::cout << "Please enter address, port, protocol and message" << std::endl;
+        return EXIT_SUCCESS;
     }
 // may crash if no ascii symbol entered;
     auto type_arg = GetString(argv[3]);
     std::transform(type_arg.begin(), type_arg.end(), type_arg.begin(),
                    [](unsigned char c) { return std::tolower(c); });
-    const auto socket = Network::NetworkHelper::MakeSocket(type_arg, GetString(argv[2]),
-                                                           GetString(argv[1]));
+    const auto socket = Network::NetworkHelper::MakeSocket(type_arg, GetString(argv[1]),
+                                                           GetString(argv[2]));
     socket->CreateSocket();
 
     auto connection = Network::NetworkHelper::MakeConnection(type_arg);
     connection->Connect(socket);
-    connection->SetBuffer(GetString(argv[4]));
-    connection->Write();
-    connection->Read();
-    const auto message = connection->GetBuffer();
-    std::cout << message << std::endl;
+    if (connection->IsConnected()) {
+        connection->SetBuffer(GetString(argv[4]));
+        connection->Write();
+        connection->Read();
+        const auto message = connection->GetBuffer();
+        std::cout << message << std::endl;
+    } else
+        std::cerr << "Connect failed." << std::endl;
     return EXIT_SUCCESS;
 }
